934d9f27fbf64923214310ce97b52a9e857604b4
Arfin Dwi Octavianto <arfindwioctavianto@gmail.com>
2023-05-29 00:05:20 +0700

    docs/add-desc&date-swagger

--- client/src/components/Expenses/ExpenseDetail.js
+++ client/src/components/Expenses/ExpenseDetail.js
@@ -29,48 +29,30 @@ const ExpenseDetail = () => {
     // minute: "numeric",
   });
 
-  const dateTimeUpdate = new Date(data?.product?.updatedAt).toLocaleString(
-    "id-ID",
-    {
-      year: "numeric",
-      month: "2-digit",
-      day: "2-digit",
-      hour: "numeric",
-      minute: "numeric",
-    }
-  );
+  const dateTimeUpdate = new Date(data?.product?.updatedAt).toLocaleString("id-ID", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "numeric",
+    minute: "numeric",
+  });
 
   return (
     <div className="container">
-      <div className="card-detail my-md-5 px-md-4 shadow-lg">
+      <div className="card-detail my-md-5 px-md-4 shadow">
         <h1 className=" text-center pt-4">Product Detail</h1>
         <div className="product-detail row d-flex px-md-4 mt-md-4">
-          <div className="img-detail col-md-5 align-items-center">
-            {data?.product?.image && (
-              <img
-                src={data.product.image}
-                alt="Product Image"
-                style={{ width: "90%" }}
-              />
-            )}
-          </div>
-          <div className="col-md-7 mt-3 mt-xl-5">
+          <div className="img-detail col-md-5 align-items-center">{data?.product?.image && <img src={data.product.image} alt="Product Image" style={{ width: "90%" }} />}</div>
+          <div className="col-md-7">
             <h1>{data?.product?.name}</h1>
-            <p className="text-danger fs-4 fw-semibold">
-              Rp. {data?.product?.price}
-            </p>
-            <p className="desc-product mx-4 mx-md-0">
-              {data?.product?.description}
-            </p>
+            <p className="text-danger fs-4 fw-semibold">Rp. {data?.product?.price}</p>
+            <p className="desc-product mx-4 mx-md-0">{data?.product?.description}</p>
             <p className="fs-5">Stock {data?.product?.stock} pcs</p>
             {/* <p className="mt-1 mb-2">Created at {dateTime} </p>  ini create dari database */}
             <p className="mt-1">Created {dateTime} </p>
             <p className="mt-1">Last Update {dateTimeUpdate} </p>
           </div>
-          <Link
-            to="/dashboard"
-            className="btn-go-dashboard mb-4 mt-md-1 btn ms-md-4 col-6 col-sm-5 col-md-4 col-xl-2"
-          >
+          <Link to="/dashboard" className="btn-go-dashboard mb-4 mt-md-4 btn ms-md-4 col-6 col-sm-5 col-md-4 col-xl-2">
             &#8810; Go to Dashboard
           </Link>
         </div>
